72372dd25566cf1e88f45db508822be0f7b52482
Brian Hague <bhague1281@gmail.com>
2020-06-01 18:36:48 -0700

    github pages deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
@@ -12,7 +12,7 @@ function App() {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>
-        
+
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
       </Router>
